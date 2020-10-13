c180902eb7acbf684810c01163cd01ccb22e876e
CarlosHenrique13 <62728022+CarlosHenrique13@users.noreply.github.com>
2020-10-13 10:22:07 -0300

    janela

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import pygame
 
+#Inicialização pygame
+pygame.init()
+display = pygame.display.set_mode([840,480])#840 x 480
+pygame.display.set_caption("Meu Super Jogo 01")
+
+gameLoop = True
+clock = pygame.time.Clock()
+while gameLoop:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameLoop = False
